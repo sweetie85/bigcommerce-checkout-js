7e28377b66c03e174f5ffd6e56c51af93b70f58d
Lalmani Dewangan <lalmani@inctechservices.com>
2025-10-21 14:10:00 +0530

    Header design

---
+++ packages/core/src/app/checkout/CheckoutSteps.tsx
@@ -0,0 +1,18 @@
+
+import React from 'react';
+
+const CheckoutSteps = () => {
+  return <div className='checkout-header test-lalmani'>
+    <div className='back-to-cart'>Return to Cart</div>
+    <div className='center-content'>
+      <div>THE ONLY WAY...</div>
+      <img alt="Byrnes and Kiefer Company - Store 1 " 
+        className="checkout-header-logo" 
+        src="https://cdn11.bigcommerce.com/s-46licyettj/product_images/download_1755843630__87459.png">
+      </img>
+      <div>FRESH EVERY DAY!</div>
+    </div>
+  </div>
+}
+
+export default CheckoutSteps;
