cfe351c3d1f6fecf7a6a05117184c7cc01c161f3
Lalmani Dewangan <lalmani@inctechservices.com>
2026-07-30 18:03:47 +0530

    Desktop footer

--- packages/core/src/app/checkout/custom/sections/CheckoutFooter.tsx
+++ packages/core/src/app/checkout/custom/sections/CheckoutFooter.tsx
@@ -3,76 +3,118 @@ import React from "react";
 const CheckoutFooter = () => {
 
   return (<footer className="site-footer">
-      <div className="footer-container">
-        {/* CONTACT */}
-        <div className="footer-col">
-          <h4>CONTACT</h4>
+    <section className="footer-info" style={{ textAlign: 'center'}}>
 
-          <p className="footer-item">
+      <article className="footer-info-col footer-info-col--small accordion-item mobile-hidden" data-section-type="storeInfo">
 
-            <svg width="20" height="20" viewBox="0 0 24 24" fill="white" xmlns="http://www.w3.org/2000/svg">
-              <path d="M6.62 10.79a15.05 15.05 0 006.59 6.59l2.2-2.2a1 1 0 011.01-.24c1.12.37 2.33.57 3.58.57a1 1 0 011 1V20a1 1 0 01-1 1C10.85 21 3 13.15 3 3a1 1 0 011-1h3.5a1 1 0 011 1c0 1.25.2 2.46.57 3.59a1 1 0 01-.25 1.01l-2.2 2.19z"/>
-            </svg>
-
-            <a href="tel:18004475797">1 (800) 447 - 5797</a>
-          </p>
-
-          <p className="footer-item">
-            <svg width="20" height="20" viewBox="0 0 24 24" fill="white" xmlns="http://www.w3.org/2000/svg">
-              <path d="M12 2C8.13 2 5 5.13 5 9c0 5.25 7 13 7 13s7-7.75 7-13c0-3.87-3.13-7-7-7zm0 9.5A2.5 2.5 0 1112 6a2.5 2.5 0 010 5.5z"/>
-            </svg>
- 
-            <span>
-              819 Baker Road <br />
-              High Point, NC 27263
-            </span>
-          </p>
-
-          <p className="footer-item">
-            <svg width="20" height="20" viewBox="0 0 24 24" fill="white" xmlns="http://www.w3.org/2000/svg">
-              <path d="M20 4H4c-1.1 0-2 .9-2 2v12c0 1.1.9 2 2 2h16c1.1 0 2-.9 2-2V6c0-1.1-.9-2-2-2zm0 4l-8 5-8-5V6l8 5 8-5v2z"/>
-            </svg>
-
-            <a href="mailto:customerservice@carolinacookie.com">
-              customerservice@carolinacookie.com
-            </a>
-          </p>
-        </div>
-
-        {/* SHOP */}
-        <div className="footer-col">
-          <h4>SHOP</h4>
-          <ul>
-            <li><a href="#">About Us</a></li>
-            <li><a href="#">Corporate Cookie Gifts</a></li>
-            <li><a href="#">Shipping & Returns</a></li>
-            <li><a href="#">Contact Us / Request A Catalog</a></li>
-            <li><a href="#">Blog</a></li>
-            <li><a href="#">Sitemap</a></li>
-          </ul>
-        </div>
-
-        {/* CAROLINA COOKIE */}
-        <div className="footer-col">
-          <h4>CAROLINA COOKIE</h4>
-          <ul>
-            <li><a href="#">Shop All</a></li>
-            <li><a href="#">Occasions</a></li>
-            <li><a href="#">Gift Types</a></li>
-            <li><a href="#">Shop By Price</a></li>
-          </ul>
-        </div>
-
-        {/* NEWSLETTER */}
+        <img src="https://cdn11.bigcommerce.com/s-fr5gzufqwh/images/stencil/original/image-manager/footer-logo.png?t=1774418815" style={{width:'106px', marginBottom:'20px'}}></img>
         <div className="footer-col footer-newsletter">
-          <h4>SWEETEN YOUR INBOX</h4>
+          <h3 style={{ display: 'flex', marginBottom: '12px' }} className="footer-info-heading" data-uw-rm-heading="prs">SWEETEN YOUR INBOX</h3>
           <form onSubmit={(e) => e.preventDefault()}>
             <input type="email" placeholder="enter your email" />
-            <button type="submit">SIGN UP</button>
+            <div>
+              <button type="submit">SIGN UP</button>
+            </div>
           </form>
         </div>
-      </div>
-    </footer>
+
+        <div style={{display:'flex', columnGap:'5px', marginTop: '12px'}}>
+            <p>
+              <a href="https://www.facebook.com/CarolinaCookieCompany/" target="_blank" style={{padding:0}}>
+                <img 
+                  src="https://cdn11.bigcommerce.com/s-fr5gzufqwh/images/stencil/original/image-manager/facebook.png?t=1759727008" 
+                  style={{width:'28px'}}>
+                </img>
+              </a>
+            </p>
+            <p>
+              <a href="https://www.instagram.com/carolina_cookie_company/?hl=en" target="_blank" style={{padding:0}} >
+                <img 
+                  src="https://cdn11.bigcommerce.com/s-fr5gzufqwh/images/stencil/original/image-manager/instagram.png?t=1759727006" 
+                  style={{width:'28px'}}>
+                </img>
+              </a>
+            </p> 
+        </div>
+      </article>
+
+      <article className="footer-info-col footer-info-col--small accordion-item" data-section-type="storeInfo">
+                    <h3 style={{ display: 'flex' }} className="footer-info-heading" data-uw-rm-heading="prs">CONTACT</h3>
+                    
+                    <div style={{ display: 'flex', columnGap: '10px', color: 'white', fontSize: '12px', marginBottom:'15px'}}>
+                        <img src="https://cdn11.bigcommerce.com/s-fr5gzufqwh/images/stencil/original/image-manager/layer-1.png?t=1746646926" style={{width:'17px', height: '17px'}} role="presentation" data-uw-rm-alt-original="null" alt="" data-uw-rm-alt-hash="3338908027751811" data-uw-rm-alt="SM" />
+                        <p style={{margin:0}} data-alpine-devtools-right-click="">1 (800) 447 - 5797</p>
+                    </div>
+                    
+                    <div style={{ display: 'flex', columnGap: '10px', color: 'white', fontSize: '12px', marginBottom:'15px', alignItems: 'center'}}>
+                        <img src="https://cdn11.bigcommerce.com/s-fr5gzufqwh/images/stencil/original/image-manager/layer-1-3-.png?t=1746646912" style={{width:'17px', height: '17px'}} role="presentation" data-uw-rm-alt-original="null" alt="" data-uw-rm-alt-hash="3338908027751811" data-uw-rm-alt="SM" />
+                        <p style={{margin:0}}>819 Baker Road<br/>
+High Point, NC 27263</p>
+                    </div>
+                    
+                    <div style={{ display: 'flex', columnGap: '10px', color: 'white', fontSize: '12px', marginBottom:'15px',  alignItems: 'center'}}>
+                        <img src="https://cdn11.bigcommerce.com/s-fr5gzufqwh/images/stencil/original/image-manager/layer-1-2-.png?t=1746646920" style={{width:'17px', height: '17px'}} role="presentation" data-uw-rm-alt-original="null" alt="" data-uw-rm-alt-hash="3338908027751811" data-uw-rm-alt="SM" />
+                        <p style={{margin:0}}>customerservice@carolinacookie.com</p>
+                    </div>
+                    
+                </article>
+        <article className="footer-info-col footer-info-col--small accordion-item" data-section-type="footer-webPages">
+            <h3 className="footer-info-heading accordion-header" data-uw-rm-heading="prs">SHOP</h3>
+            <ul className="footer-info-list accordion-content">
+                    <li>
+                        <a href="https://carolinacookie.com/contact-us-tj3z/" data-uw-rm-brl="PR" data-uw-original-href="https://carolinacookie.com/contact-us-tj3z/">Contact Us</a>
+                    </li>
+                    <li>
+                        <a href="https://carolinacookie.com/faq/" data-uw-rm-brl="PR" data-uw-original-href="https://carolinacookie.com/faq/">FAQ</a>
+                    </li>
+                    <li>
+                        <a href="https://carolinacookie.com/about-us/" data-uw-rm-brl="PR" data-uw-original-href="https://carolinacookie.com/about-us/">About Us</a>
+                    </li>
+                    <li>
+                        <a href="https://carolinacookie.com/corporate-cookie-gifts/" data-uw-rm-brl="PR" data-uw-original-href="https://carolinacookie.com/corporate-cookie-gifts/">Corporate Cookie Gifts</a>
+                    </li>
+                    <li>
+                        <a href="https://carolinacookie.com/shipping-returns/" data-uw-rm-brl="PR" data-uw-original-href="https://carolinacookie.com/shipping-returns/">Shipping &amp; Returns</a>
+                    </li>
+                    <li>
+                        <a href="https://carolinacookie.com/freshness-guarantee/" data-uw-rm-brl="PR" data-uw-original-href="https://carolinacookie.com/freshness-guarantee/">Freshness Guarantee </a>
+                    </li>
+                    <li>
+                        <a href="https://carolinacookie.com/contact-us/" data-uw-rm-brl="PR" data-uw-original-href="https://carolinacookie.com/contact-us/">Contact Us / Request a Catalog</a>
+                    </li>
+                    <li>
+                        <a href="https://carolinacookie.com/blog/" data-uw-rm-brl="PR" data-uw-original-href="https://carolinacookie.com/blog/">Blog</a>
+                    </li>
+                    <li>
+                        <a href="https://carolinacookie.com/privacy-policy/" data-uw-rm-brl="PR" data-uw-original-href="https://carolinacookie.com/privacy-policy/">Privacy Policy</a>
+                    </li>
+                <li>
+                    <a href="https://carolinacookie.com/sitemap.php" data-uw-rm-brl="PR" data-uw-original-href="https://carolinacookie.com/sitemap.php">Sitemap</a>
+                </li>
+            </ul>
+        </article>
+
+        <article className="footer-info-col footer-info-col--small accordion-item" data-section-type="footer-categories">
+            <h3 className="footer-info-heading accordion-header" data-uw-rm-heading="prs">CAROLINA COOKIE</h3>
+            <ul className="footer-info-list accordion-content">
+                    <li>
+                        <a href="https://carolinacookie.com/corporate-gift/" data-uw-rm-brl="PR" data-uw-original-href="https://carolinacookie.com/corporate-gift/">Corporate Gift</a>
+                    </li>
+                    <li>
+                        <a href="https://carolinacookie.com/shop-gifts/" data-uw-rm-brl="PR" data-uw-original-href="https://carolinacookie.com/shop-gifts/">Shop Gifts</a>
+                    </li>
+                    <li>
+                        <a href="https://carolinacookie.com/occasions/" data-uw-rm-brl="PR" data-uw-original-href="https://carolinacookie.com/occasions/">Occasions</a>
+                    </li>
+                    <li>
+                        <a href="https://carolinacookie.com/recipients/" data-uw-rm-brl="PR" data-uw-original-href="https://carolinacookie.com/recipients/">Recipients</a>
+                    </li>
+                <li><a href="https://carolinacookie.com/content/2025-holidaycatalog.pdf" target="_blank" aria-label="Holiday Catalog - open in a new tab" data-uw-rm-ext-link="" uw-rm-external-link-id="https://carolinacookie.com/content/2025-holidaycatalog.pdf$holidaycatalog" data-uw-pdf-br="1" data-uw-pdf-doc="">Holiday Catalog</a></li>
+                <li><a href="https://carolinacookie.com/flavors-and-nutrition/" data-uw-rm-brl="PR" data-uw-original-href="https://carolinacookie.com/flavors-and-nutrition/">Flavors &amp; Nutrition</a></li>
+            </ul>
+        </article>
+    </section>
+</footer>
   );
 }
 
